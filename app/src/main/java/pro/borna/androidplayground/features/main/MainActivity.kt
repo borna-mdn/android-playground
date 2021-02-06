7111a60705381900826c7744df73b07a237bd36d
borna-mdn <medanic.borna@gmail.com>
2021-02-06 14:39:42 +0100

    Add main screen

---
+++ app/src/main/java/pro/borna/androidplayground/features/main/MainActivity.kt
@@ -0,0 +1,7 @@
+package pro.borna.androidplayground.features.main
+
+import androidx.appcompat.app.AppCompatActivity
+import pro.borna.androidplayground.R
+
+
+class MainActivity : AppCompatActivity(R.layout.layout_main)
